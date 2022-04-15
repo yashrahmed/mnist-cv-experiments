9865c1da6491d71a473d7a48fb60e7cde1b90468
yashrahmed <yashrahmed@gmail.com>
2022-04-14 21:36:22 -0500

    [BRIEF] Some clean up with numpy.

--- brief-features/brief-test.py
+++ brief-features/brief-test.py
@@ -3,12 +3,12 @@ from functools import partial
 import cv2
 import numpy as np
 from cv2.xfeatures2d import BriefDescriptorExtractor_create
+from numpy.linalg import norm
 
-from common.func_utils import compose_n
 from common.img_utils import build_image_lookup, show_images
 
 
-def create_brief_extractor_pipeline(desc_size=16):
+def extract_brief(images, desc_size=16):
     """
         OpenCV indirectly imposes a size restriction.
         See https://github.com/opencv/opencv_contrib/blob/342f8924cca88fe6ce979024b7776f6815c89978/modules/xfeatures2d/src/brief.cpp#L249
@@ -25,24 +25,36 @@ def create_brief_extractor_pipeline(desc_size=16):
     resize_dims = (60, 60)
     fixed_keypoint = [cv2.KeyPoint(30, 30, 0)]  # A single Keypoint fixed at the center of the image.
     extractor = BriefDescriptorExtractor_create(desc_size)
-    operations = [
-        lambda img: cv2.resize(img, resize_dims),  # Resize image to a standard size
-        lambda img: extractor.compute(img, fixed_keypoint)[1]  # extract feature for a single fixed keypoint
-    ]
-    return compose_n(operations)
+
+    brief_features = list(
+        map(lambda image: extractor.compute(cv2.resize(image, resize_dims), fixed_keypoint)[1], images))
+    return np.reshape(np.array(brief_features), (len(images), desc_size))
 
 
-def hamming_dist(v1, v2):
-    return np.sum(np.unpackbits(v1 ^ v2))
+def euclidean_dist(img1, img2):
+    return norm(img1 - img2)
+
+
+def hamming_dist(ref_vector, vectors):
+    n_rows = vectors.shape[0]
+    ref_vector_repeated = np.repeat(ref_vector, n_rows, axis=0)
+    return np.sum(np.unpackbits(ref_vector_repeated ^ vectors, axis=1), axis=1)
+
+
+def scale_distances(distances, low=0, high=10):
+    return np.interp(distances, (distances.min(), distances.max()), (low, high)).astype(np.uint8)
 
 
 if __name__ == '__main__':
-    extract_brief = create_brief_extractor_pipeline()
+    # extract_brief = create_brief_extractor_pipeline()
     lookup = build_image_lookup()
-    idxs = [(3, 41), (3, 45), (3, 23), (3, 34), (1, 1), (2, 21), (4, 11), (5, 15), (6, 56), (7, 36), (8, 25), (9, 44)]
+    idxs = [(7, 30), (3, 45), (3, 23), (3, 34), (1, 1), (2, 21), (4, 11), (5, 15), (6, 56), (7, 36), (8, 25), (9, 44)]
     images = list(map(lookup, idxs))
-    features = list(map(extract_brief, images))
-    distances = list(map(partial(hamming_dist, features[0]), features))
-    print('-------------- hamming distances --------------')
-    print(distances)
+    features = extract_brief(images)
+    feature_ham_distances = hamming_dist(features[[0]], features)
+    img_euc_distance = np.array(list(map(partial(euclidean_dist, images[0]), images)))
+    print('-------------- Hamming distances b/w feature --------------')
+    print(scale_distances(feature_ham_distances))
+    print('-------------- Euclidean distance b/w feature --------------')
+    print(scale_distances(img_euc_distance))
     show_images(images)
