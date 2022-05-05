[SHAPE-CONTEXT] Bug fix in feature calculation + implenebnting distance metric.

shape_context/contour-expr.py
@@ -5,7 +5,7 @@ from sklearn.cluster import AgglomerativeClustering
 from common.dataset_utils import load_actual_mnist
 from common.plot_utils import scatter_plot
 
-from shape_context_desc import compute_descriptor as get_sc
+from shape_context_desc import compute_descriptor as get_sc, compute_cost_matrix, compute_cost_matrix_raw
 
 
 def threshold_image(images, threshold=100):
@@ -22,8 +22,8 @@ def run_clustering_on_image(image):
 
 
 def run_clustering_based_sampling_on_image(image, n_clusters=10):
-    sample_points, uniq_labels = sample_points_using_clustering(image, n_clusters)
-    plt = scatter_plot(sample_points, uniq_labels)
+    sample_points = sample_points_using_clustering(image, n_clusters)
+    plt = scatter_plot(sample_points, np.arange(0, n_clusters))
     return plt
 
 
@@ -48,25 +48,33 @@ def sample_points_using_clustering(image, n_clusters=10):
     agg = AgglomerativeClustering(n_clusters=n_clusters, linkage='average')
     agg.fit(points)
 
-    uniq_labels = np.unique(agg.labels_)
-    sampled_points = np.array([np.mean(points[agg.labels_ == label], axis=0) for label in uniq_labels])
-    return sampled_points, uniq_labels
+    unique_labels = np.unique(agg.labels_)
+    sampled_points = np.array([np.mean(points[agg.labels_ == label], axis=0) for label in unique_labels])
+    return sampled_points
 
 
 if __name__ == '__main__':
     train_images, train_labels, _, _ = load_actual_mnist()
-    image_of_6 = train_images[train_labels == 4][1914]
-    image_of_6_col = cv2.cvtColor(image_of_6, cv2.COLOR_GRAY2BGR)
-    th_image = threshold_image(image_of_6)
-    # det_contours, _ = get_contours(th_image)
+    image_of_4 = threshold_image(train_images[train_labels == 4][1914])
+    image_of_42 = threshold_image(train_images[train_labels == 4][194])
+    image_of_5 = threshold_image(train_images[train_labels == 5][119])
+
+    # det_contours, _ = get_contours(image_of_4)
     # polygons = get_polygons(det_contours)
-    # img = draw_polygons_on_image(image_of_6, polygons)
-    run_clustering_on_image(th_image)
-    plt_handle = run_clustering_based_sampling_on_image(th_image)
+    # img = draw_polygons_on_image(image_of_4, polygons)
+    run_clustering_on_image(image_of_5)
+    plt_handle = run_clustering_based_sampling_on_image(image_of_5)
     plt_handle.show()
     # img = cv2.drawContours(image_of_6_col, det_contours, -1, (0, 0, 255), 1)
     # show_image(cv2.resize(img, (300, 300)))
 
-    sample_points, _ = sample_points_using_clustering(th_image)
-    descs = get_sc(sample_points)
-    print(descs)
+    descs_4 = get_sc(sample_points_using_clustering(image_of_4))
+    descs_42 = get_sc(sample_points_using_clustering(image_of_42))
+    descs_5 = get_sc(sample_points_using_clustering(image_of_5))
+    #
+    mat = compute_cost_matrix(descs_4, descs_42)
+    mat2 = compute_cost_matrix(descs_4, descs_5)
+
+    print(mat)
+    print('*******************')
+    print(mat2)

shape_context/shape_context_desc.py
@@ -1,7 +1,33 @@
+from math import pi
+
 import numpy as np
 from numpy import arctan2, histogram2d
 from scipy.spatial.distance import pdist, squareform
-from math import pi
+
+
+def compute_cost_matrix(desc1, desc2):
+    r1, c1 = desc1.shape
+    r2, c2 = desc2.shape
+    desc1 = desc1.reshape([r1, 1, c1])
+    desc2 = desc2.reshape([1, r2, c2])
+    numr = np.power(desc1 - desc2, 2)
+    denr = desc1 + desc2
+    cost_mat = np.sum(np.divide(numr, np.where(denr > 0, denr, 0.01)), axis=2)
+    return cost_mat / 2
+
+
+# For debugging purposes only!
+def compute_cost_matrix_raw(desc1, desc2):
+    mat = []
+    for r1 in desc1:
+        costs = []
+        for r2 in desc2:
+            numr = np.power(r1 - r2, 2)
+            denr = r1 + r2
+            denr = np.where(denr > 0, denr, 0.01)
+            costs.append(np.sum(np.divide(numr, denr)))
+        mat.append(costs)
+    return np.array(mat) / 2
 
 
 def compute_descriptor(vec, d_bin=5, t_bin=12):
@@ -9,7 +35,7 @@ def compute_descriptor(vec, d_bin=5, t_bin=12):
     d_inner = 1
     d_outer = 40
     t_start = 0
-    t_end = 2*pi
+    t_end = 2 * pi
     d_bin_edges = np.logspace(np.log10(d_inner), np.log10(d_outer), d_bin)
     t_bin_edges = np.linspace(t_start, t_end, t_bin)
 
@@ -23,7 +49,7 @@ def get_pairwise_dists(vec):
     # vec is a NX2 array.
     n, _ = vec.shape
     dists = squareform(pdist(vec))
-    return dists[np.where(dists > 0)].reshape([n, -1])
+    return dists.reshape([n, -1])
 
 
 def get_pairwise_slopes(vec):
@@ -33,4 +59,4 @@ def get_pairwise_slopes(vec):
     dx = xs.transpose() - xs
     dy = ys.transpose() - ys
     angles = arctan2(dy, dx)
-    return angles[np.where(np.abs(angles) > 0)].reshape([n, -1]) + pi
+    return angles.reshape([n, -1]) + pi