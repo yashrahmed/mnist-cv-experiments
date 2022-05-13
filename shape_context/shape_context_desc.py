f9b952f357f28a2ee901c51fa18e32c1730dff35
yashrahmed <yashrahmed@gmail.com>
2022-05-12 22:31:15 -0500

    [SHAPE-CONTEXT] Normalizaing the histogram.

--- shape_context/shape_context_desc.py
+++ shape_context/shape_context_desc.py
@@ -35,7 +35,7 @@ def compute_cost_matrix(desc1, desc2):
     desc2 = desc2.reshape([1, r2, c2])
     numr = np.power(desc1 - desc2, 2)
     denr = desc1 + desc2
-    cost_mat = np.sum(np.divide(numr, np.where(denr > 0, denr, 0.01)), axis=2)
+    cost_mat = np.sum(np.divide(numr, denr + 0.01), axis=2)
     return cost_mat / 2
 
 
@@ -70,8 +70,11 @@ def compute_descriptor(vec, d_bin=6, t_bin=13):
     angles = get_pairwise_slopes(vec)
     median_dist = np.median(dists)
     dists = dists / median_dist
-    descs = np.array([histogram2d(dists[i, :], angles[i, :], bins=[d_bin_edges, t_bin_edges])[0] for i in range(n)])
-    return descs.reshape([n, -1]), median_dist
+    descs = []
+    for i in range(n):
+        histogram = histogram2d(dists[i, :], angles[i, :], bins=[d_bin_edges, t_bin_edges])[0]
+        descs.append(histogram / (np.sum(histogram) + 0.01))
+    return np.array(descs).reshape([n, -1]), median_dist
 
 
 def get_pairwise_dists(vec):
